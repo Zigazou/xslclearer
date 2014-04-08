e914f98d4a5dbe9668772de6e5deea14ba3bdb11
Frédéric BISSON <zigazou@free.fr>
2014-04-08 22:11:04 +0200

    Better error handling for xslclearer.py Ported xslsproc from Bash to Python to handle xsltproc options

--- xslclearer.py
+++ xslclearer.py
@@ -25,7 +25,13 @@ def offset_to_column_line(text, offset):
     return (row + 1, column + 1)
 
 def xsls_compile(xsls_file):
-    """Converts an .xsls file into an .xslt file"""
+    """Converts an .xsls file into an .xslt file
+    
+    It returns a tuple (return_code, output_xsls)
+    return_code == 0 : compilation OK
+    return_code == 1 : lexical analysis failed
+    return_code == 2 : grammar analysis failed
+    """
     xslstext = xsls_file.read()
 
     try:
@@ -35,24 +41,43 @@ def xsls_compile(xsls_file):
         tokens = [token for token in tokenizer.tokenize(xslstext, skips)]
 
         compiler = xsls.Compiler(tokens)
-        return compiler.compile()
+        return (0, compiler.compile())
     except xsls.TokenizerException as exception:
         row, column = offset_to_column_line(xslstext, exception.offset)
-        return "{message} at row {row}, column {column}".format(
+        return (1, "{message} at row {row}, column {column}".format(
             message=exception.message,
             row=row,
             column=column
-        )
+        ))
     except xsls.CompilerException as exception:
         row, column = offset_to_column_line(xslstext, exception.offset)
-        return "{message} at row {row}, column {column}".format(
+        return (2, "{message} at row {row}, column {column}".format(
             message=exception.message,
             row=row,
             column=column
-        )
+        ))
+
+def main():
+    """Interface between the operating system and the compiler"""
+
+    # Determine where the input comes from
+    if len(sys.argv) != 2:
+        input_stream = sys.stdin
+    else:
+        input_stream = open(sys.argv[1])
+
+    # Compile the XSLS input
+    code, output = xsls_compile(input_stream)
+
+    # Determine where the output has to go
+    if code == 0:
+        output_stream = sys.stdout
+    else:
+        output_stream = sys.stderr
+
+    print(output, file=output_stream)
+    sys.exit(code)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print(xsls_compile(sys.stdin))
-    else:
-        print(xsls_compile(open(sys.argv[1])))
+    main()
+
